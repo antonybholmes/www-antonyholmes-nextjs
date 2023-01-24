9bd845b6d303d8768bff3cb159a2a151adbc80ed
Antony Holmes <antony@antonybholmes.com>
2023-01-24 18:07:23 -0500

    Bug fixes and updates.

--- src/components/header/menu-overlay.tsx
+++ src/components/header/menu-overlay.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useRef } from "react"
-import { ANIMATION_DURATION_S } from "../../constants"
 import SmallLogoIcon from "../../icons/logo-icon-small"
 import cn from "../../lib/class-names"
 import BaseLink from "../link/base-link"
@@ -7,7 +5,6 @@ import VCenterRow from "../v-center-row"
 import IHeaderProps from "./header-props"
 import MenuOpenButton from "./menu-button-open"
 import MenuLinks from "./menu-links"
-import { gsap, Power3 } from "gsap"
 
 export interface IMenuOverlayProps extends IHeaderProps {
   showMenu: boolean
@@ -20,109 +17,105 @@ export default function MenuOverlay({
   showMenu,
   onClick,
 }: IMenuOverlayProps) {
-  const overlayRef = useRef(null)
-  const sideMenuRef = useRef(null)
+  // const overlayRef = useRef(null)
+  // const sideMenuRef = useRef(null)
 
-  useEffect(() => {
-    animateMenu()
-  }, [showMenu])
-
-  function animateMenu() {
-    if (showMenu) {
-      gsap
-        .timeline()
-        .set(overlayRef.current, {
-          visibility: "visible",
-        })
-        .to(
-          overlayRef.current,
-          {
-            duration: ANIMATION_DURATION_S,
-            opacity: 1,
-            ease: Power3.easeOut,
-          },
-          0
-        )
-        .to(
-          sideMenuRef.current,
-          {
-            duration: ANIMATION_DURATION_S,
-            opacity: 1,
-            ease: Power3.easeOut,
-          },
-          0
-        )
-        .to(
-          sideMenuRef.current,
-          {
-            x: 0,
-            duration: ANIMATION_DURATION_S,
-            ease: Power3.easeOut,
-          },
-          0
-        )
-    } else {
-      gsap
-        .timeline()
-        .to(
-          overlayRef.current,
-          {
-            duration: ANIMATION_DURATION_S,
-            opacity: 0,
-            ease: Power3.easeOut,
-          },
-          0
-        )
-        .to(
-          overlayRef.current,
-          {
-            duration: ANIMATION_DURATION_S,
-            opacity: 0,
-            ease: Power3.easeOut,
-          },
-          0
-        )
-        .to(
-          sideMenuRef.current,
-          {
-            duration: ANIMATION_DURATION_S,
-            opacity: 0,
-            ease: Power3.easeOut,
-          },
-          0
-        )
-        .to(
-          sideMenuRef.current,
-          {
-            x: "-4rem",
-            duration: ANIMATION_DURATION_S,
-            ease: Power3.easeOut,
-          },
-          0
-        )
-        .set(overlayRef.current, {
-          visibility: "hidden",
-        })
-    }
-  }
+  // useEffect(() => {
+  //   if (showMenu) {
+  //     gsap
+  //       .timeline()
+  //       .set(overlayRef.current, {
+  //         visibility: "visible",
+  //       })
+  //       .to(
+  //         overlayRef.current,
+  //         {
+  //           duration: ANIMATION_DURATION_S,
+  //           opacity: 1,
+  //           ease: Power3.easeOut,
+  //         },
+  //         0
+  //       )
+  //       .to(
+  //         sideMenuRef.current,
+  //         {
+  //           duration: ANIMATION_DURATION_S,
+  //           opacity: 1,
+  //           ease: Power3.easeOut,
+  //         },
+  //         0
+  //       )
+  //       .to(
+  //         sideMenuRef.current,
+  //         {
+  //           x: 0,
+  //           duration: ANIMATION_DURATION_S,
+  //           ease: Power3.easeOut,
+  //         },
+  //         0
+  //       )
+  //   } else {
+  //     gsap
+  //       .timeline()
+  //       .to(
+  //         overlayRef.current,
+  //         {
+  //           duration: ANIMATION_DURATION_S,
+  //           opacity: 0,
+  //           ease: Power3.easeOut,
+  //         },
+  //         0
+  //       )
+  //       .to(
+  //         overlayRef.current,
+  //         {
+  //           duration: ANIMATION_DURATION_S,
+  //           opacity: 0,
+  //           ease: Power3.easeOut,
+  //         },
+  //         0
+  //       )
+  //       .to(
+  //         sideMenuRef.current,
+  //         {
+  //           duration: ANIMATION_DURATION_S,
+  //           opacity: 0,
+  //           ease: Power3.easeOut,
+  //         },
+  //         0
+  //       )
+  //       .to(
+  //         sideMenuRef.current,
+  //         {
+  //           x: "-4rem",
+  //           duration: ANIMATION_DURATION_S,
+  //           ease: Power3.easeOut,
+  //         },
+  //         0
+  //       )
+  //       .set(overlayRef.current, {
+  //         visibility: "hidden",
+  //       })
+  //   }
+  // }, [showMenu])
 
   return (
     <div
-      ref={overlayRef}
+      //ref={overlayRef}
       className={cn(
-        "fixed left-0 top-0 z-100 flex h-screen w-full flex-row bg-black/70 backdrop-blur-sm overflow-hidden"
-        // [
-        //   showMenu,
-        //   "overlay-show opacity-100 visible",
-        //   "overlay-hide opacity-0 invisible",
-        // ]
+        "fixed left-0 top-0 z-100 flex h-screen w-full flex-row bg-black/70 backdrop-blur-sm overflow-hidden",
+        [
+          showMenu,
+          "overlay-show opacity-100 visible",
+          "overlay-hide opacity-0 invisible",
+        ]
       )}
     >
       <div
-        ref={sideMenuRef}
+        //ref={sideMenuRef}
         className={cn(
-          "h-full w-72 bg-white trans-ani-300 transition-translation"
-          // [showMenu, "ml-0", "-ml-8"]
+          "h-full w-72 bg-white trans-ani-300 transition-translation",
+          [showMenu, "ml-0", "-ml-8"]
         )}
       >
         <VCenterRow className="gap-x-2 px-5 py-3">
