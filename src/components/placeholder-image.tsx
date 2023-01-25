5ade17d1bfd69ac6c465d8d25426bfa813ffabb8
Antony Holmes <antony@antonybholmes.com>
2023-01-25 16:50:44 -0500

    Bug fixes and updates.

--- src/components/placeholder-image.tsx
+++ src/components/placeholder-image.tsx
@@ -73,7 +73,7 @@ export default memo(function PlaceholderImage({
           height={size[1]}
           className={cn(
             "w-full trans-ani-700 transition-placeholder",
-            [isLoaded, "blur-none opacity-100", "blur-lg opacity-90"],
+            [isLoaded, "blur-none", "blur-lg"],
             className,
             imgClassName
           )}
